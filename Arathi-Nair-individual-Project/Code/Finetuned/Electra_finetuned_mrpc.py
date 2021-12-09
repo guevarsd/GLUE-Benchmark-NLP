c71b96503a32034a0aae9d99c59107bafd67963f
arathinair11 <74162219+arathinair11@users.noreply.github.com>
2021-12-09 15:34:45 -0500

    Arathi-Nair-Individual-Report

---
+++ Arathi-Nair-individual-Project/Code/Finetuned/Electra_finetuned_mrpc.py
@@ -0,0 +1,128 @@
+import torch
+torch.cuda.empty_cache()
+from datasets import load_dataset
+from transformers import ElectraTokenizer, DataCollatorWithPadding,DataCollatorForLanguageModeling,TrainingArguments,\
+    Trainer
+from transformers import ElectraForSequenceClassification
+from datasets import load_metric
+import os
+import numpy as np
+
+
+# List of glue tasks
+GLUE_TASKS = ["cola", "mnli", "mnli-mm", "mrpc", "qnli", "qqp", "rte", "sst2", "stsb", "wnli"]
+task = "mrpc"
+model_checkpoint = "google/electra-small-discriminator"
+batch_size = 8
+
+# Load dataset based on task variable
+actual_task = "mnli" if task == "mnli-mm" else task
+dataset = load_dataset("glue", actual_task)
+metric = load_metric('glue', actual_task)
+
+# Create tokenizer for respective model
+tokenizer = ElectraTokenizer.from_pretrained(model_checkpoint, use_fast=True)
+
+task_to_keys = {
+    "cola": ("sentence", None),
+    "mnli": ("premise", "hypothesis"),
+    "mnli-mm": ("premise", "hypothesis"),
+    "mrpc": ("sentence1", "sentence2"),
+    "qnli": ("question", "sentence"),
+    "qqp": ("question1", "question2"),
+    "rte": ("sentence1", "sentence2"),
+    "sst2": ("sentence", None),
+    "stsb": ("sentence1", "sentence2"),
+    "wnli": ("sentence1", "sentence2"),
+}
+
+# Select columns with sentences to tokenize based on given task
+sentence1_key, sentence2_key = task_to_keys[task]
+def tokenizer_func(examples):
+    if sentence2_key is None:
+        return tokenizer(examples[sentence1_key], truncation=True)
+    return tokenizer(examples[sentence1_key], examples[sentence2_key], truncation=True)
+
+# Tokenize sentence(s)
+encoded_dataset = dataset.map(tokenizer_func, batched=True)
+data_collator = DataCollatorForLanguageModeling( tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+# Number of logits to output
+num_labels = 3 if task.startswith("mnli") else 1 if task=="stsb" else 2
+
+# Create model and attach ForSequenceClassification head
+model = ElectraForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels)
+
+# Type of metric for given task
+metric_name = "pearson" if task == "stsb" else "matthews_correlation" if task == "cola" else "accuracy"
+model_name = model_checkpoint.split("/")[-1]
+
+args = TrainingArguments(
+    f"{model_checkpoint}-fintuned-{task}",
+    evaluation_strategy = "epoch",
+    save_strategy = "epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model=metric_name,
+    #eval_accumulation_steps=5,
+
+)
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    if task != "stsb":
+        predictions = np.argmax(predictions, axis=1)
+    else:
+        predictions = predictions[:, 0]
+
+    return metric.compute(predictions=predictions, references=labels)
+
+validation_key = "validation_mismatched" if task == "mnli-mm" else "validation_matched" if task == "mnli" else "validation"
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=encoded_dataset["train"],
+    eval_dataset=encoded_dataset[validation_key],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics
+)
+
+trainer.train()
+trainer.evaluate()
+
+# Finetune Using Optuna
+
+import optuna
+# ! pip install optuna
+
+def model_init():
+    return ElectraForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels)
+
+
+trainer = Trainer(
+    model_init=model_init,
+    args=args,
+    train_dataset = encoded_dataset["train"],#shard(index=1, num_shards=10),
+    eval_dataset=encoded_dataset[validation_key],
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+best_run = trainer.hyperparameter_search(n_trials=10, direction="maximize", backend="optuna")
+
+for n, v in best_run.hyperparameters.items():
+    setattr(trainer.args, n, v)
+
+trainer.train()
+
+# Save final model
+print(os.getcwd())
+path = 'Electra_finetuned'+task
+model.save_pretrained(path)
+
+
